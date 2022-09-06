6484c8421b3e68eeda96b5a59b8b287d7fa79ab6
Caio F <caiofsousa27@hotmail.com>
2022-09-06 13:51:38 -0300

    Modulo 5

--- src/app/components/photo-list/photo-list.component.ts
+++ src/app/components/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Photo } from 'src/app/shared/components/photo-board/interfaces/photo';
 import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
 import {Observable} from 'rxjs'
+import {faCircleNotch} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-photo-list',
@@ -10,6 +11,7 @@ import {Observable} from 'rxjs'
 })
 export class PhotoListComponent implements OnInit{
   public photos$: Observable<Photo[]>;
+  public fa = {faCircleNotch}
 
   constructor(private service: PhotoBoardService) {}
 
